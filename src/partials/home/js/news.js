d29ee0c5fa38be85b74a313e4dc624c1610c0244
samir-sb <samir.belhadi@univ-batna2.dz>
2020-11-03 22:32:31 +0100

    change user

--- src/partials/home/js/news.js
+++ src/partials/home/js/news.js
@@ -24,10 +24,10 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination',
     clickable: true,
   },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
